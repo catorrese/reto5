6fcd8a4ce79dd3ee0494b0796cf2aaa365477488
catorrese <ca.torrese@uniandes.edu.co>
2021-02-27 18:29:33 -0500

    Avance 2

--- index.js
+++ index.js
@@ -3,16 +3,38 @@ let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-const tb = 
-
+const table = document.getElementById('eventos');
 request.onload = function() {
-    const events = request.response;
-    eventsTable(events);
+    if (request.status == 200){
+        const events = request.response;
+        eventsTable(events);
+    } 
   }
 
-function eventsTable(obj){
-    for(let i = 0; i < obj.lenght; i++){
-        console.log(obj[i]);
+function eventsTable(lista){
+    
+    for(let i = 0; i < 90; i++){ 
+        const entrada = lista[i];
+        const tr = document.createElement('tr');
+        const col1 = document.createElement('th');
+        const col2 = document.createElement('td');
+        const col3 = document.createElement('td');
+        const eventos = entrada['events'];
+        const squirrel = entrada.squirrel
+        let texto = "";
+        for(var j in eventos){
+            texto += eventos[j] + ',';
+        }
+        texto = texto.slice(0, texto.length-1)
+        col1.textContent = i+1;
+        col2.textContent = texto;
+        col3.textContent = squirrel;
+
+        tr.appendChild(col1);
+        tr.appendChild(col2);
+        tr.appendChild(col3);
+
+        table.appendChild(tr);
     }
 
 }
